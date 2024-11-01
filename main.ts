ef55e80e56eaf48e265de3211df4b41ef7e600bf
Daniel Ajeeson <164007636+githubaccu@users.noreply.github.com>
2024-11-01 19:50:24 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # . # . .
+        # . # . .
+        # . # # #
+        # # . # #
+        # . # . .
+        `)
+    basic.showLeds(`
+        . . # # #
+        # # # # .
+        # # . . #
+        # # # # #
+        . # # # #
+        `)
+    basic.showNumber(5756554335856749000)
+    basic.showNumber(6.567456758697686e+30)
+    basic.showNumber(76587453464576560000)
+    basic.showString("HGJDYDYUGYUFTXGFI7GI86FI77I")
+    basic.showString("YF7F8H9IH98YF875F78I88FY")
+    basic.showIcon(IconNames.Meh)
+    basic.showIcon(IconNames.Umbrella)
+    basic.showString("G7IF5D697H08F67D57EDYG08")
+})
+basic.forever(function () {
+    basic.showString("JDFBGUSERHBGIUSHERGUIHEGIHEGRUIGHSUIGHSUIHG9UH89URSHY8")
+    basic.showNumber(7.698769807698058e+56)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showIcon(IconNames.No)
+    basic.showString("8")
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        # # # # #
+        # # . # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # #
+        . # # . .
+        . . # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . # . .
+        . . . . #
+        . # # # .
+        # # . # #
+        `)
+})
